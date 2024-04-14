0ae46704e0e674920fffb86c260b8b0ae8c0a77f
Abir Majumdar <102758397+majumdarabir@users.noreply.github.com>
2024-04-14 22:16:47 +0530

    Update Home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -29,16 +29,16 @@ export default function Home() {
     <div>
       <Navbar />
       <div className='flex-auto flex flex-wrap justify-start'>
-        {data.map((item, index) => (
-          <div className="card" style={{ width: 300, margin: 10 }} key={index}>
-            <img src={item.course_pic} className="card-img-top" alt="..." style={{ maxWidth: '100%', height: 300, padding: 5 }} />
-            <div className="card-body">
-              <h5 className="card-title"><b>{item.course_name}</b></h5>
-              <p className="card-text">{item.course_desc}</p>
-              <a href="#" className="btn btn-primary" style={{ marginTop: 5 }}>Go inside</a>
-            </div>
-          </div>
-        ))}
+        // {data.map((item, index) => (
+        //   <div className="card" style={{ width: 300, margin: 10 }} key={index}>
+        //     <img src={item.course_pic} className="card-img-top" alt="..." style={{ maxWidth: '100%', height: 300, padding: 5 }} />
+        //     <div className="card-body">
+        //       <h5 className="card-title"><b>{item.course_name}</b></h5>
+        //       <p className="card-text">{item.course_desc}</p>
+        //       <a href="#" className="btn btn-primary" style={{ marginTop: 5 }}>Go inside</a>
+        //     </div>
+        //   </div>
+        // ))}
       </div>
       <Footer/>
     </div>
